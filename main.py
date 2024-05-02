2bb5b0a0d457d47fe9b3cd9d44493e063b6c2e73
hao <ahmed.ait.ouazzou@protonmail.com>
2024-05-02 20:13:05 +0200

    Added function to compute Treynor ratio

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from src import compute_risk_adjusted_return
 # Example usage:
 security_returns = [12.5, 5.1, 15.2, -24.25, -5.85, 10]
 benchmark_returns = [10, 8, 12, -20, -3, 9]
+risk_free_rate = [3, 1, -1, 5, 2.4, 8.5]
 
-from src.compute_metric import beta
 
-a = beta([0.10, 0.20, 0.5, 0.20], [0.10, 0.20, 0.5, 0.20])
+a = compute_risk_adjusted_return.jensen_alpha(security_returns, benchmark_returns)
 print(a)
